9cb7dd8d788c041771c2cd04e1fd1f9efb3873ba
Emaani-gh <abdulsalamseidu@protonmail.com>
2023-01-26 23:00:52 +0200

    updated logo link

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
-      <div class="logo-info"><a href="/about">Seidu</a></div>
+      <div class="logo-info"><Link to="/about">Seidu</Link></div>
       <div class="hamburger-menu" id="hamburger"><i onClick={()=>console.log("u clicked")} class="fa fa-bars"></i></div>
     </header>
   )

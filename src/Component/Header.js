3b964a42aa7e044c055032367e81cf991c7b58a2
Emaani-gh <abdulsalamseidu@protonmail.com>
2023-01-26 23:10:29 +0200

    updated about me

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-      <div class="logo-info"><Link to="/about">Seidu</Link></div>
+      <div class="logo-info"><Link to="/">Seidu</Link></div>
       <div class="hamburger-menu" id="hamburger"><i onClick={()=>console.log("u clicked")} class="fa fa-bars"></i></div>
     </header>
   )
